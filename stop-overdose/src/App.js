2990c7e18b825ba4cd7bbf8f754f7eeb064242a8
Alex Lee <al8876@gmail.com>
2018-03-23 17:49:10 -0700

    added google map to page

--- stop-overdose/src/App.js
+++ stop-overdose/src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 49.2827, lng: -123.1207 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 49.2827, lng: -123.1207 }} />}
+  </GoogleMap>
+))
+
+
 class App extends Component {
+
+  
   render() {
     return (
       <div className="App">
@@ -13,7 +27,16 @@ class App extends Component {
         <p className="App-intro">
           First: call 911.
         </p>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
+
+
     );
   }
 }
