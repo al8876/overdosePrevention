251945736305688cd5b0bf716de9391fc3f70008
Alex Lee <al8876@gmail.com>
2018-03-23 18:28:47 -0700

    directional map deleted

--- stop-overdose/src/App.js
+++ stop-overdose/src/App.js
@@ -10,60 +10,24 @@ const onMarkerClick = () => {
   alert('hello')
 }
 
-// const MyMapComponent = compose(
-//   withProps({
-//     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-//     loadingElement: <div style={{ height: `100%` }} />,
-//     containerElement: <div style={{ height: `400px` }} />,
-//     mapElement: <div style={{ height: `100%` }} />,
-//   }),
-//   withScriptjs,
-//   withGoogleMap
-//   )((props) =>
-//   <GoogleMap
-//     defaultZoom={10}
-//     defaultCenter={{ lat: 49.2827, lng: -123.1207 }}
-//   >
-//     {props.isMarkerShown && <Marker position={{ lat: 49.2827, lng: -123.1207 }} onClick={this.onMarkerClick} />}
-//   </GoogleMap>
-// )
-
-const MapWithADirectionsRenderer = compose(
+const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
-  withGoogleMap,
-  lifecycle({
-    componentDidMount() {
-      const DirectionsService = new google.maps.DirectionsService();
-
-      DirectionsService.route({
-        origin: new google.maps.LatLng(41.8507300, -87.6512600),
-        destination: new google.maps.LatLng(41.8525800, -87.6514100),
-        travelMode: google.maps.TravelMode.DRIVING,
-      }, (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-          this.setState({
-            directions: result,
-          });
-        } else {
-          console.error(`error fetching directions ${result}`);
-        }
-      });
-    }
-  })
-)(props =>
+  withGoogleMap
+  )((props) =>
   <GoogleMap
-    defaultZoom={7}
-    defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}
+    defaultZoom={10}
+    defaultCenter={{ lat: 49.2827, lng: -123.1207 }}
   >
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
+    {props.isMarkerShown && <Marker position={{ lat: 49.2827, lng: -123.1207 }} onClick={this.onMarkerClick} />}
   </GoogleMap>
-);
+)
+
 
 
 
@@ -112,8 +76,7 @@ class App extends Component {
         Place hand under head to support the head.
         Place the top leg slightly in front of the leg touching the ground, and place the knee to the ground to prevent the body from rolling into the stomach.
       </p>
-        {/* <MyMapComponent isMarkerShown /> */}
-        <MapWithADirectionsRenderer />
+        <MyMapComponent isMarkerShown />
       </div>
 
 
