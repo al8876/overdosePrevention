cc56dc6904e886d764066609b7638208891a303d
Alex Lee <al8876@gmail.com>
2018-03-23 17:51:55 -0700

    removed logo and made header 50px

--- stop-overdose/src/App.js
+++ stop-overdose/src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Stop Overdose BC</h1>
         </header>
         <p className="App-intro">
