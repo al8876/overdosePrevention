450762cfcd6516cdb72c082a7ad1df9b12174be4
hvdson <hvdsonmusic@gmail.com>
2018-03-23 18:23:41 -0700

    refactored shit into component

--- stop-overdose/src/App.js
+++ stop-overdose/src/App.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import MyMapComponent from './components/GoogleMaps.js'
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: 49.2827, lng: -123.1207 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: 49.2827, lng: -123.1207 }} />}
-  </GoogleMap>
-))
-
+// const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+//   <GoogleMap
+//     defaultZoom={8}
+//     defaultCenter={{ lat: 49.2827, lng: -123.1207 }}
+//   >
+//     {props.isMarkerShown && <Marker position={{ lat: 49.2827, lng: -123.1207 }} />}
+//   </GoogleMap>
+// ))
 
 class App extends Component {
-
-  
   render() {
     return (
       <div className="App">
@@ -27,16 +26,8 @@ class App extends Component {
         <p className="App-intro">
           First: call 911.
         </p>
-        <MyMapComponent
-          isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
+        <MyMapComponent/>
       </div>
-
-
     );
   }
 }
